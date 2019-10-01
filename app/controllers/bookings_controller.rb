ed223688bedd22d1e2dca1354fb84f778d689752
Braxton Lemmon <braxtonlemmon@gmail.com>
2019-10-01 08:48:31 +0200

    Add show page for created booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -16,6 +16,13 @@ class BookingsController < ApplicationController
 		end
 	end
 
+	def show
+		@booking = Booking.find(params[:id])
+		@flight = Flight.find(@booking.flight_id)
+		@from = Airport.find(@flight.from_airport_id)
+		@to = Airport.find(@flight.to_airport_id)
+	end
+
 	private
 
 	def booking_params
