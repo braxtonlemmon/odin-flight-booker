e258e838bc769c70c205d92f4b8d9b6dc8f7bafa
Braxton Lemmon <braxtonlemmon@gmail.com>
2019-10-02 07:08:53 +0200

    Add layout partials. Complete app functionality.

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,23 +1,33 @@
 class BookingsController < ApplicationController
+	before_action :verify_flight_selected, only: :new
+	# before_action :verify_passenger_info, only: :create
+
 	def new
-		if params[:select_flight][:flight_id]
-			@booking = Booking.new
-			@flight = Flight.find(params[:select_flight][:flight_id])
-			params[:select_flight][:passengers].to_i.times do
-				@booking.passengers.build
-			end
-		else
-			flash.now[:danger] = 'You must select a flight!'
-			redirect_to root_url
-		end
+		# @booking = Booking.new
+		@flight = Flight.find(params[:booking][:flight_id])
+		@booking = @flight.bookings.new(booking_params)
+		params[:booking][:passengers].to_i.times { @booking.passengers.build }
+		# if params[:booking][:flight_id]
+		# 	@booking = Booking.new
+		# 	@flight = Flight.find(params[:booking][:flight_id])
+		# 	params[:booking][:passengers].to_i.times { @booking.passengers.build }
+		# else
+		# 	flash[:danger] = 'You must select a flight!'
+		# 	redirect_to root_url
+		# end
 	end
 
 	def create
-		@booking = Booking.new(booking_params)
+		@flight = Flight.find(params[:booking][:flight_id])
+		@booking = @flight.bookings.new(booking_params)
+
 		if @booking.save
+			flash[:success] = 'Your flight has been booked!'
 			redirect_to @booking
 		else
-			redirect_to root_path
+			flash.now[:success] = 'Please fill out passenger details.'
+			puts 'no no'
+			render 'new'
 		end
 	end
 
@@ -34,4 +44,20 @@ class BookingsController < ApplicationController
 		params.require(:booking).permit(:flight_id,
 																		passengers_attributes: [:id, :name, :email])
 	end
+
+	def verify_flight_selected
+		unless params[:booking][:flight_id]
+			flash[:warning] = 'You did not select a flight!'
+			redirect_to root_path
+		end
+	end
+
+	# def verify_passenger_info
+	# 	params[:booking][:passengers].to_i.times do |i|
+			
+	# 	unless @booking.passengers.all? { |passenger| passenger.valid? }
+	# 		flash.now[:danger] = 'Please fill out all passenger info'
+	# 		render 'new'
+	# 	end
+	# end
 end
